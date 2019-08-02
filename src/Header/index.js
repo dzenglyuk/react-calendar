fe750abe7bf3d0de1d35ff7a0923c6a0f927b067
Oleh Dzenhliuk <odzen@softserveinc.com>
2019-08-02 16:58:17 +0300

    First

---
+++ src/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function Header() {
+  return (
+    <div className="Header">
+        <span className="header-name"> Event calendar </span>
+        <div className="menu">
+        </div>
+    </div>
+  );
+}
+
+export default Header;
